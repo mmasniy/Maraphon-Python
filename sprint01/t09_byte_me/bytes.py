d3b6178f77001950767a0efc5dd0b45594ba2b56
mmasniy <masnijmaksim@gmail.com>
2021-05-11 09:19:24 +0300

    update task 09z

--- sprint01/t09_byte_me/bytes.py
+++ sprint01/t09_byte_me/bytes.py
@@ -1,12 +1,8 @@
 def convert_to_bytes(arg_int, arg_bool, arg_str):
-    print(f'-- The int value is {arg_int}\n'
+    arg2_bool = (False, True)["True" == arg_bool]
+    print(f'-- The int value is "{arg_int}"\n'
           f'bytes: "{bytes(int(arg_int))}"')
-    print(f'-- The bool value is {arg_bool}\n'
-          f'bytes: "{bytes(bool(arg_bool))}"')
-    print(f'-- The string value is {arg_str}\n'
+    print(f'-- The bool value is "{arg_bool}"\n'
+          f'bytes: "{bytes(arg2_bool)}"')
+    print(f'-- The string value is "{arg_str}"\n'
           f'bytes: "{bytes(arg_str, "utf-8")}"')
-
-
-convert_to_bytes('10', 'False', 'aaaa')
-convert_to_bytes('5', 'True', 'Are you suggesting that coconuts migrate?')
-convert_to_bytes('1', 'true', 'bbb')
