a9b0c109ae148826f2c13c696fa4846f8dc58fb6
mmasniy <masnijmaksim@gmail.com>
2021-05-28 12:35:17 +0300

    delete print in t08

--- sprint02/t08_contains/contains.py
+++ sprint02/t08_contains/contains.py
@@ -2,9 +2,7 @@ def contains(all_string, substrings):
     result = list()
     all_string = str.lower(all_string)
     for item in substrings:
-        print(item)
         sub_line = str.lower(item)
-        print(item)
         if all_string.find(sub_line) != -1:
             result.append(item)
 
